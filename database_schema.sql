a5b8eeb472ffb194ed00d3d9df37dd3a84514752
Lenin <153633104+leninramadass@users.noreply.github.com>
2024-09-21 00:26:36 +0530

    Add files via upload

---
+++ database_schema.sql
@@ -0,0 +1,11 @@
+-- Query to select all records from the BOOK table
+SELECT
+    *
+FROM
+    BOOK;
+
+-- Query to count the total number of records in the BOOK table
+SELECT
+    COUNT(*)
+FROM
+    BOOK;
